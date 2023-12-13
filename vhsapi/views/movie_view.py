f752af4bb0cb1f42ace8c628ab2c0358883ac23e
Scott Cole <sscole929@gmail.com>
2023-12-13 13:45:54 -0600

    Created and loaded fixtures

--- vhsapi/views/movie_view.py
+++ vhsapi/views/movie_view.py
@@ -1,15 +1,31 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from vhsapi.models import Movie
+from vhsapi.models import Movie, MovieGenre
+from .genre_view import GenreSerializer
 from django.core.exceptions import PermissionDenied
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genre_data = serializers.SerializerMethodField()
     """JSON serializer for types"""
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'release_year', 'starring', 'director', 'genre', 'production_studio', 'cover_img_url', 'is_available')
+        fields = ('id', 'title', 'release_year', 'starring', 'director', 'genre_data', 'production_studio', 'cover_img_url', 'is_available')
+
+    def get_genre_data(self, movie):
+        genre_name = movie.genre.all()
+        return GenreSerializer(genre_name, many=True).data
+
+
+    def create(self, correct_data):
+        genre_nums = correct_data.pop('genre_nums', [])
+        movie = Movie.objects.create(**correct_data)
+
+        for genre_num in genre_nums:
+            MovieGenre.objects.create(movie=movie, genre_num=genre_num)
+
+        return movie
 
 class MovieView(ViewSet):
     def list(self, request):
@@ -30,3 +46,12 @@ class MovieView(ViewSet):
             return Response(serialized.data, status=status.HTTP_200_OK)
         except Movie.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+    def create(self, request):
+
+        serialized = MovieSerializer(data=request.data)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
